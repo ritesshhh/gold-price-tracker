cdbfa65ca4b1b49a40760e6e8306a9ace562b25c
Ritesh Kr <recklessritesh@gmail.com>
2025-08-09 00:16:16 +0530

    Update track_gold_price.py

--- track_gold_price.py
+++ track_gold_price.py
@@ -13,7 +13,10 @@ HEADERS = {"User-Agent": "Mozilla/5.0"}
 
 
 def fetch_gold_rate():
-    headers = {"User-Agent": "Mozilla/5.0"}
+    # headers = {"User-Agent": "Mozilla/5.0"}
+    headers = {
+    "User-Agent": "Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/115.0 Safari/537.36"
+    }
     response = requests.get(GRT_URL, headers=headers)
     soup = BeautifulSoup(response.content, "html.parser")
     print("soup.pretty", soup.prettify())
